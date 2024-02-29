0c04064a96fbfe8fef19dad10dfdd5beff0308ae
Matheus Araújo de Carvalho - 822223290 <822223290@ulife.com.br>
2024-02-28 21:13:27 -0300

    05 - Adicionado novo arquivo .gitignore

--- Empregado.java
+++ Empregado.java
@@ -11,7 +11,7 @@ public class Empregado{
         if (tipo == 1){
             return salario;
         } else if (tipo == 2) {
-            return (salario + salario)*comissao;
+            return salario + salario *comissao;
         } else if (tipo == 3){
             return salario + bonus;
         } else {
@@ -43,6 +43,30 @@ public class Empregado{
 
     public void setTipo(int tipo){
         this.tipo = tipo;
+    }
+
+    public double getSalario(){
+        return this.salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+    public double getComissao(){
+        return this.comissao;
+    }
+
+    public void setComissao(double comissao){
+        this.comissao = comissao;
+    }
+
+    public double getBonus(){
+        return this.bonus;
+    }
+
+    public void setBonus(double bonus){
+        this.bonus = bonus;
     }
 
 }
